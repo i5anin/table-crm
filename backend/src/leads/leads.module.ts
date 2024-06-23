a446d8bbcfb5be4722da3cee6a2c3af17de1fe9f
Isanin Sergey <sergey.isanin@gmail.com>
2024-06-23 15:33:40 +0300

    ref

--- backend/src/leads/leads.module.ts
+++ backend/src/leads/leads.module.ts
@@ -2,18 +2,17 @@ import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('api/leads')
 export class LeadsController {
-    @Get()
-    findAll() {
-        // логика для обработки GET запроса
-        return 'This action returns all leads';
-    }
+  @Get()
+  findAll() {
+    // логика для обработки GET запроса
+    return 'This action returns all leads';
+  }
 
-    @Post()
-    create() {
-        // логика для обработки POST запроса
-        return 'This action adds a new lead';
-    }
+  @Post()
+  create() {
+    // логика для обработки POST запроса
+    return 'This action adds a new lead';
+  }
 }
 
-export class LeadsModule {
-}
+export class LeadsModule {}
