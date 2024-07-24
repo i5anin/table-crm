0e2474a39a5b2438259dcf6a8dd617d21eeba050
Isanin Sergey <sergey.isanin@gmail.com>
2024-07-24 18:49:38 +0300

    +

--- backend/src/leads/amo-crm.service.ts
+++ backend/src/leads/amo-crm.service.ts
@@ -1,21 +1,34 @@
 import { Injectable, Inject } from '@nestjs/common';
-// Использование импорта * as AmoCRM для импортирования всего содержимого пакета
 import { ConfigService } from '@nestjs/config';
-import AmoCRM from 'amocrm-api'; // Если AmoCRM - дефолтный экспорт
+import AmoCRM from 'amocrm-api';
 
 @Injectable()
 export class AmoCRMService {
-  private readonly amo: AmoCRM; // Использование типа AmoCRM для amo
+  private readonly amo: AmoCRM;
 
   constructor(
     @Inject(ConfigService)
     private readonly configService: ConfigService,
   ) {
-    this.amo = new AmoCRM({
+    // Выводим значения из .env в консоль
+
+    // Проверяем тип baseUrl
+    const baseUrl = this.configService.get<string>('AMOCRM_BASE_URL');
+    if (typeof baseUrl !== 'string') {
+      console.error('Ошибка: baseUrl должен быть строкой.');
+      // Добавьте сюда логику для обработки ошибки
+      return; // Или throw error;
+    }
+
+    // Создайте экземпляр AmoCRM вне конструктора
+    const amo = new AmoCRM({
+      baseUrl, // Используем baseUrl
       clientId: this.configService.get<string>('AMOCRM_CLIENT_ID'),
       clientSecret: this.configService.get<string>('AMOCRM_CLIENT_SECRET'),
       redirectUri: this.configService.get<string>('AMOCRM_REDIRECT_URI'),
     });
+    // Задайте this.amo после создания экземпляра
+    this.amo = amo;
   }
 
   async findAllLeads(query: string = '') {
@@ -23,8 +36,22 @@ export class AmoCRMService {
       const result = await this.amo.api.deals.get({ query });
       return result;
     } catch (error) {
-      console.error('Ошибка при получении сделок из AMOCRM', error);
-      throw error;
+      // Обработка ошибок API AmoCRM
+      if (error.response && error.response.status) {
+        // Ошибка HTTP-запроса
+        console.error(
+          `Ошибка API AmoCRM: ${error.response.status} - ${error.response.statusText}`,
+        );
+        console.error('Ответ сервера:', error.response.data);
+        // Дополнительная обработка ошибок HTTP
+        // Например, проверка кода ошибки и обработка соответствующим образом
+      } else {
+        // Другая ошибка, например, ошибка сети
+        console.error('Ошибка при обращении к API AmoCRM:', error);
+      }
+      // Дополнительные действия при ошибке
+      // Например, возврат пустого массива или кастомной ошибки
+      return [];
     }
   }
 }
